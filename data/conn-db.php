16e7ec720dd1652222822d7f257792be0bfd213b
NKumar-coder21 <62969623+NKumar-coder21@users.noreply.github.com>
2022-07-05 13:27:32 -0500

    Update conn-db.php

--- data/conn-db.php
+++ data/conn-db.php
@@ -1,10 +1,10 @@
 <?php
 //Regular PHP
-private $servername = "n4m3x5ti89xl6czh.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
-private $DBuser = "t7xs9wf15xm0s7ql";
-private $DBpass = "s1poz3dm8p209okx";
-private $DBname = "smzomjz5osq3jb94";
-private $port = "3306";
+$servername = "n4m3x5ti89xl6czh.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
+$DBuser = "t7xs9wf15xm0s7ql";
+$DBpass = "s1poz3dm8p209okx";
+$DBname = "smzomjz5osq3jb94";
+$port = "3306";
 $success = mysqli_connect(
     $servername,
     $DBuser,
