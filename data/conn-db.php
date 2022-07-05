2417c1cb13fe271868275f75500141bfc0fbc50e
NKumar-coder21 <62969623+NKumar-coder21@users.noreply.github.com>
2022-07-05 13:24:15 -0500

    Update conn-db.php

--- data/conn-db.php
+++ data/conn-db.php
@@ -1,18 +1,16 @@
 <?php
 //Regular PHP
-$servername = "localhost";
-$DBuser = "root";
-$DBpass = "root";
-$DBname = "Wearhouse";
-$port = "8889";
-$DBsock = "/Applications/MAMP/tmp/mysql/mysql.sock";
+private $servername = "n4m3x5ti89xl6czh.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
+private $DBuser = "t7xs9wf15xm0s7ql";
+private $DBpass = "s1poz3dm8p209okx";
+private $DBname = "smzomjz5osq3jb94";
+private $port = "3306";
 $success = mysqli_connect(
     $servername,
     $DBuser,
     $DBpass,
     $DBname,
-    $port,
-    $DBsock
+    $port
 );
 if (!$success) {
     die("Connection to DB failed: " . mysqli_connect_error());
