b025640c268045f4c156185f053916b8628ce19e
Nathaniel Kumar <62969623+NKumar-coder21@users.noreply.github.com>
2022-07-05 14:16:11 -0500

    Change of style

--- account/login.php
+++ account/login.php
@@ -7,7 +7,7 @@
         <title> Apostolic Men Wear-House | Login</title>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="stylesheet" href="../styles/logstyle.css" />
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" />
     </head>
 
     <body style="background-color: #102132">
