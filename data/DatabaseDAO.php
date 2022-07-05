bd2d7687393b19f1b678e134d8f61990da2f44fc
NKumar-coder21 <62969623+NKumar-coder21@users.noreply.github.com>
2022-07-05 13:01:48 -0500

    Update DatabaseDAO.php
    Connect to Heroku database

--- data/DatabaseDAO.php
+++ data/DatabaseDAO.php
@@ -1,11 +1,11 @@
 <?php
 //DAO
 class DatabaseDAO {
-private $servername = "localhost";
-private $DBuser = "root";
-private $DBpass = "root";
-private $DBname = "Wearhouse";
-private $port = "8889";
+private $servername = "n4m3x5ti89xl6czh.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
+private $DBuser = "t7xs9wf15xm0s7ql";
+private $DBpass = "s1poz3dm8p209okx";
+private $DBname = "smzomjz5osq3jb94";
+private $port = "3306";
     function getConnection() {
         
         $conn = mysqli_connect(
